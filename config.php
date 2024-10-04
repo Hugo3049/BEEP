08bf009c37e689220300f394c58fb50c869877a3
Hugo Tabak <107777605+Hugo3049@users.noreply.github.com>
2024-10-04 13:31:15 +0200

    Update

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 $_ENV['DB_HOST'] = 'localhost';
 $_ENV['DB_NAME'] = 'assets_insight';
-$_ENV['DB_USER'] = 'bit_academy';
-$_ENV['DB_PASS'] = 'bit_academy';
+$_ENV['DB_USER'] = 'user';
+$_ENV['DB_PASS'] = 'pass';
 
 $_ENV['LANG'] = 'nl';
 
